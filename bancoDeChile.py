7f1d637191542a024529eeaa2e84459621679324
Martín Araya <mbozo@uc.cl>
2024-06-20 19:58:14 -0400

    Niow mo ranom

--- bancoDeChile.py
+++ bancoDeChile.py
@@ -5,6 +5,7 @@ import json
 from datetime import datetime
 import re
 import locale
+import random
 
 # Configurar el locale para español
 locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
@@ -88,34 +89,84 @@ def insert_discounts(data, category_ids, card_ids):
         elif item.get("category", "").startswith("beneficios/mascota"):
             category = category_ids["Mascotas"]
         lista = item.get("cards").split(",")
+        if item.get("bank") == "Banco de Chile":
+            if item.get("cards") == "":
+                cards = [["Bronze","Debito"],["Silver","Credito"],["Gold","Credito"]]
+                for card in cards:
+                    discount = {
+                        "url": item.get("url"),
+                        "local": item.get("title"),
+                        "discount": extract_discount(item.get("excerpt")),
+                        "description": item.get("description"),
+                        "category": category,
+                        "expiration": expiration_date,
+                        "days": days,
+                        "card": card_ids[card[0]],
+                        "cardType": card[0],
+                        "paymentType": card[1],
+                        "bankName": "Banco de Chile"
+                    }
+                    discountsTable.insert_one(discount)
+            else:
+                for elemento in lista:
+                    if elemento =="Bronze":
+                        type = "Debito"
+                    else:
+                        type = "Credito"
 
-        if item.get("cards") == "":
-            cards = ["Bronze", "Silver", "Gold"]
-            for card in cards:
-                discount = {
-                    "url": item.get("url"),
-                    "local": item.get("title"),
-                    "discount": extract_discount(item.get("excerpt")),
-                    "description": item.get("description"),
-                    "category": category,
-                    "expiration": expiration_date,
-                    "days": days,
-                    "card": card_ids[card]
-                }
-                discountsTable.insert_one(discount)
+                    discount = {
+                        "url": item.get("url"),
+                        "local": item.get("title"),
+                        "discount": extract_discount(item.get("excerpt")),
+                        "description": item.get("description"),
+                        "category": category,
+                        "expiration": expiration_date,
+                        "days": days,
+                        "card": card_ids[elemento],
+                        "cardType":elemento,
+                        "paymentType": type,
+                        "bankName": "Banco de Chile"
+                    }
+                    discountsTable.insert_one(discount)
         else:
-            for elemento in lista:
-                discount = {
-                    "url": item.get("url"),
-                    "local": item.get("title"),
-                    "discount": extract_discount(item.get("excerpt")),
-                    "description": item.get("description"),
-                    "category": category,
-                    "expiration": expiration_date,
-                    "days": days,
-                    "card": card_ids[elemento]
-                }
-                discountsTable.insert_one(discount)
+            if item.get("cards") == "":
+                cards = [["Black", "Debito"], ["Diamond", "Credito"]]
+                for card in cards:
+                    discount = {
+                        "url": item.get("url"),
+                        "local": item.get("title"),
+                        "discount": extract_discount(item.get("excerpt")),
+                        "description": item.get("description"),
+                        "category": category,
+                        "expiration": expiration_date,
+                        "days": days,
+                        "card": card_ids[card[0]],
+                        "cardType": card[0],
+                        "paymentType": card[1],
+                        "bankName": "Banco Santander"
+                    }
+                    discountsTable.insert_one(discount)
+            else:
+                for elemento in lista:
+                    if elemento == "Black":
+                        type = "Debito"
+                    else:
+                        type = "Credito"
+
+                    discount = {
+                        "url": item.get("url"),
+                        "local": item.get("title"),
+                        "discount": extract_discount(item.get("excerpt")),
+                        "description": item.get("description"),
+                        "category": category,
+                        "expiration": expiration_date,
+                        "days": days,
+                        "card": card_ids[elemento],
+                        "cardType": elemento,
+                        "paymentType": type,
+                        "bankName": "Banco Santander"
+                    }
+                    discountsTable.insert_one(discount)
 def insert_categories():
     categories = ["Comida", "Transporte", "Bienestar", "Mascotas"]
     category_ids = {}
@@ -128,20 +179,24 @@ def insert_categories():
     return category_ids
 
 def insert_banks():
-    banks = ["Banco de Chile"]
+    banks = ["Banco de Chile", "Banco Santander"]
+    bank_ids = {}
     for bank in banks:
-        bank = {
+        banco = {
             "name": bank
         }
-        result = banksTable.insert_one(bank)
-    return result.inserted_id
+        result = banksTable.insert_one(banco)
+        bank_ids[bank] = str(result.inserted_id)
+    return bank_ids
 
-def insert_cards(bank_id):
-    cards = [["Bronze","Debito"],["Silver","Credito"],["Gold","Credito"]]
+def insert_cards(bank_ids):
+    cards = [["Bronze","Debito","Banco de Chile"],["Silver","Credito","Banco de Chile"],
+             ["Gold","Credito","Banco de Chile"], ["Black","Debito","Banco Santander"],
+             ["Diamond", "Credito","Banco Santander"]]
     card_ids = {}
     for card in cards:
         tarjeta = {
-            "bankId": bank_id,
+            "bankId": bank_ids[card[2]],
             "cardType": card[0],
             "paymentMethod": card[1]
         }
@@ -159,8 +214,9 @@ categoriesTable.delete_many({})
 cardsTable.delete_many({})
 banksTable.delete_many({})
 usersTable.delete_many({})
-bank_id = insert_banks()
+bank_ids = insert_banks()
 category_ids = insert_categories()
-card_ids = insert_cards(bank_id)
+print(category_ids)
+card_ids = insert_cards(bank_ids)
 insert_discounts(data,category_ids,card_ids)
 
